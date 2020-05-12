f7e9ad3a94712f97fe529088fd6f664f44b13eb0
lee-hees <2sdsf00@naver.com>
2020-05-12 12:09:05 +0900

    start project

---
+++ soft3_101/app/src/main/java/dsu/software/soft3_101/MainActivity.java
@@ -0,0 +1,14 @@
+package dsu.software.soft3_101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
