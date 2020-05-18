9592ac2f9d5d674911cf4ba44b2c00473078a8e4
lee-hees <2sdsf00@naver.com>
2020-05-18 22:59:03 +0900

    정렬 및 수정

--- soft3_101/app/src/main/java/dsu/software/soft3_101/MainActivity.java
+++ soft3_101/app/src/main/java/dsu/software/soft3_101/MainActivity.java
@@ -1,4 +1,4 @@
-package dsu.software.subwaybusan;
+package dsu.software.soft3_101;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,54 +21,54 @@ import org.w3c.dom.Text;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
     ListView listView;
-            List<String> mList;
+    List<String> mList;
+    TextView first;
 
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        listView = (ListView)findViewById(R.id.list_view);
-       String strs[] = {
-               getString(R.string.first),
-               getString(R.string.second),
-               getString(R.string.third),
-               getString(R.string.fourth),
-               getString(R.string.dong)
+        listView = (ListView) findViewById(R.id.list_view);
+        String[] strs = {
+                getString(R.string.first),
+                getString(R.string.second),
+                getString(R.string.third),
+                getString(R.string.fourth),
+                getString(R.string.dong)
 
-       };
+        };
 
         mList = Arrays.asList(strs);
 
         listView.setAdapter(new MyCustomAdapter());
 
+    }
+
+
+    /*** Custom Adapter ***/
+    private class MyCustomAdapter extends ArrayAdapter<String> {
+
+        public MyCustomAdapter() {
+            super(MainActivity.this, R.layout.item_layout, mList);
         }
 
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null)
+                convertView = getLayoutInflater().inflate(R.layout.item_layout, parent, false);
 
-/*** Custom Adapter ***/
-private class MyCustomAdapter extends ArrayAdapter<String>
-{
+            TextView textViewAtCustomLayout = (TextView) convertView.findViewById(R.id.text_view_at_custom_layout);
+            textViewAtCustomLayout.setText(mList.get(position));
 
-    public MyCustomAdapter()
-    {
-        super(MainActivity.this,R.layout.item_layout,mList);
+            return convertView;
+        }
     }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent)
-    {
-        if(convertView==null) convertView = getLayoutInflater().inflate(R.layout.item_layout,parent,false);
-
-        TextView textViewAtCustomLayout = (TextView)convertView.findViewById(R.id.text_view_at_custom_layout);
-        textViewAtCustomLayout.setText(mList.get(position));
-
-        return convertView;
-    }
-}
 
 }
