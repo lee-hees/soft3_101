2a48c904eb9b740aac35af1f2f78c60e95dd0664
lee-hees <2sdsf00@naver.com>
2020-06-01 11:30:36 +0900

    로그인 페이지

--- Fragment_test/app/src/main/java/dsu/software/fragment_test/MainActivity.java
+++ Fragment_test/app/src/main/java/dsu/software/fragment_test/MainActivity.java
@@ -5,7 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +21,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView login =findViewById(R.id.login);
+
+        final EditText id = findViewById(R.id.id);
+        final EditText pw = findViewById(R.id.pw);
+        final Button login = findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent login = new Intent(getApplicationContext(), Station_Tab.class);
-                startActivity(login);
+
+                if (id.getText().toString().equals("admin") && pw.getText().toString().equals("admin")) {
+                    Intent login = new Intent(getApplicationContext(), Station_Tab.class);
+                    startActivity(login);
+                }
+                else if(!id.getText().toString().equals("admin")){
+                    Toast.makeText(MainActivity.this, "계정을 확인하세요", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "비밀번호를 확인하세요", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
